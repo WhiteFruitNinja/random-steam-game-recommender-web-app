6849618da27bfbcdac42d45b222a619db0b477d7
WhiteFruitNinja <whitefruitninja@gmail.com>
2025-01-20 16:59:20 +0200

    Added frontend(React) and backend(Spring)

---
+++ random-steam-game-generator-backend/src/main/java/com/example/random_steam_game_recommender/controller/MainController.java
@@ -0,0 +1,9 @@
+package com.example.random_steam_game_recommender.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class MainController {
+}
---
+++ random-steam-game-generator-backend/src/main/java/com/example/random_steam_game_recommender/model/UserModel.java
@@ -0,0 +1,10 @@
+package com.example.random_steam_game_recommender.model;
+
+public class UserModel {
+    private String username;
+    private String email;
+    private String password;
+    private String passwordConfirm;
+
+
+}
